8c8e6c0049d2ddbce840ce735f9e26bac3fa0d87
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-07-18 12:54:53 -0300

    adicionando api consulta cep

---
+++ api consulta/apiconsulta.js
@@ -0,0 +1,31 @@
+const preencherFormulario = (endereco) => {
+    let elemento = document.querySelector('li')
+    elemento.textContent = 'CEP: '+endereco.cep+ ', Bairro ' + endereco.bairro+', '+endereco.logradouro + ', na cidade de ' + endereco.localidade+'-'+endereco.uf
+    document.querySelector('#cep').value=""
+    
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero)
+const cepValido = (cep) => cep.length == 8 && eNumero(cep)
+
+const pesquisarCEP = async () => {
+    const cep = document.getElementById('cep').value
+    const url = `http://viacep.com.br/ws/${cep}/json/`
+    if (cepValido(cep)) {
+        const dados = await fetch(url)//await permite esperar para resolver paso a paso
+        const endereco = await dados.json()
+        if (endereco.hasOwnProperty('erro')) {
+            document.querySelector('li').textContent = 'CEP não encontrado'
+        } else { preencherFormulario(endereco) }
+    } else {
+        document.querySelector('li').textContent = 'CEP incorreto'
+
+    }
+}
+
+document.querySelector('#botaoRegistrar').addEventListener('click', function (evt) {//adicionando eventListener
+    evt.preventDefault()
+})
+document.getElementById('botaoRegistrar')
+    .addEventListener('click', pesquisarCEP)
